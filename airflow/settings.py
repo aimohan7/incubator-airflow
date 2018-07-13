f1309ff863d9c7c12f989f1b0652bb878de1fbc6
Aishwarya Mohan <aimohan@paypal.com>
2018-07-12 21:14:48 -0700

    Added worker initialisation precheck - connection to metadata database

--- airflow/settings.py
+++ airflow/settings.py
@@ -27,7 +27,7 @@ import logging
 import os
 import pendulum
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import NullPool
 
@@ -140,6 +140,26 @@ def configure_vars():
     AIRFLOW_HOME = os.path.expanduser(conf.get('core', 'AIRFLOW_HOME'))
     SQL_ALCHEMY_CONN = conf.get('core', 'SQL_ALCHEMY_CONN')
     DAGS_FOLDER = os.path.expanduser(conf.get('core', 'DAGS_FOLDER'))
+
+
+def validate_session():
+    try:
+        worker_precheck = conf.getboolean('core', 'worker_precheck')
+    except conf.AirflowConfigException:
+        worker_precheck = False
+    if not worker_precheck:
+        return True
+    else:
+        check_session = sessionmaker(bind=engine)
+        session = check_session()
+        try:
+            session.execute("select 1")
+            conn_status = True
+        except exc.DBAPIError as err:
+            log.error(err)
+            conn_status = False
+        session.close()
+        return conn_status
 
 
 def configure_orm(disable_connection_pool=False):
